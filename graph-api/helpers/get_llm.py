a5ab3e8a4c5f94a76fde72013aa8270383f6c477
Dawid Drożdżyński <dawid.drozdzynski.g4@gmail.com>
2026-03-23 18:05:29 +0100

    remove dotenv

--- graph-api/helpers/get_llm.py
+++ graph-api/helpers/get_llm.py
@@ -1,9 +1,7 @@
 import os
-from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 
 def get_llm():
-    load_dotenv()
     llm = ChatGroq(
         temperature=0,
         model_name=os.getenv("MODEL_NAME"),
